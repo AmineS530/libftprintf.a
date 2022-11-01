ca88370d5afd574306901a57cdc74f07f9e0197c
Amine Sadik <asadik@e2r4p8.1337.ma>
2022-11-01 13:47:20 +0100

    added different versions of putnbr

--- ft_putnbr_fd_hex.c
+++ ft_putnbr_fd_hex.c
@@ -1,40 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr_fd_hex.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asadik <asadik@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/30 16:33:19 by asadik            #+#    #+#             */
-/*   Updated: 2022/11/01 13:46:19 by asadik           ###   ########.fr       */
+/*   Created: 2022/11/01 13:15:42 by asadik            #+#    #+#             */
+/*   Updated: 2022/11/01 13:30:55 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	coversions();
-
-
-int	ft_printf(const char *param_name, ...)
+void	ft_putnbr_fd_hex(int n, char x, int fd)
 {
-	va_list	ap;
+	int		base;
+	char	*hex;
 
-	va_start(ap , param_name[i]);
-	while (param_name[i]!= '%' and param_name[i])
+	base = 16;
+	if (x == 'x')
+		hex = HEX_LOWER;
+	else
+		hex = HEX_UPPER;
+	if (n < base)
+		ft_putchar_fd(hex[n % base], FD);
+	else
 	{
-		if (l3iba[i] == '%')
-		{
-			if (l3iba[i + 1] == '%')
-				ft_putchar_fd ("%", FD);
-			if (l3iba[i + 1] == 'd' || l3iba[i + 1] == 'i')
-				ft_putnbr_fd(l3iba[i], FD);
-		}
-		else
-		{
-			ft_putchar_fd(param_name[i], FD);
-		}
-		i++;
-	}
+		ft_putnbr_fd_hex((n / base), x , FD);
+		ft_putnbr_fd_hex((n % base), x , FD);
 
-	va_end(ap);
+	}
 }
--- ft_putnbr_fd_unsigned.c
+++ ft_putnbr_fd_unsigned.c
@@ -1,27 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_putnbr_fd_unsigned.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asadik <asadik@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/30 16:22:54 by asadik            #+#    #+#             */
-/*   Updated: 2022/11/01 13:32:05 by asadik           ###   ########.fr       */
+/*   Created: 2022/11/01 12:57:58 by asadik            #+#    #+#             */
+/*   Updated: 2022/11/01 13:05:20 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-# define FD 1
-# define HEX_LOWER "0123456789abcdef"
-# define HEX_UPPER "0123456789ABCDEF"
-# include <stdarg.h>
-# include <unistd.h>
+#include <ft_printf.h>
 
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putnbr_fd_unsigned(unsigned int n, int fd);
-void	ft_putnbr_fd_hex(int n, char x, int fd);
-
-#endif
+void	ft_putnbr_fd_unsigned(unsigned int n, int fd)
+{
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	if (n >= 0 && n <= 9)
+		ft_putchar_fd(n + '0', fd);
+}
